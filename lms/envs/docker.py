f3c23b170d6655970426598206e33d9a89380e9e
Morgan Robertson <robertson.md7@gmail.com>
2015-05-18 23:16:12 -0500

    Configure email.

--- lms/envs/docker.py
+++ lms/envs/docker.py
@@ -5,7 +5,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.mandrillapp.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "nate@appsembler.com"
+EMAIL_HOST_USER = "robertson.md7@gmail.com"
 EMAIL_HOST_PASSWORD = os.environ.get("MANDRILL_API_KEY", "")
 
 LMS_BASE = os.environ.get("EDX_LMS_BASE", "")
